4e876cb1b6434d411aa0effd9d910e9a777b717a
John Lee <escavecha@gmail.com>
2023-08-21 04:38:06 -0400

    work in progress

--- bike_share.R
+++ bike_share.R
@@ -10,7 +10,7 @@ library(gridExtra)
 library(reshape2)
 
 # NOTE: set the working directory accordingly
-setwd("case1")
+setwd("C:/Google_DA_CaseStudy")
 
 # import csv data files
 Trips_2019_Q1 <- read_csv("Divvy_Trips_2019_Q1.csv")
